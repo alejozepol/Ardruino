09d1f3934921b69a788250cdd38aa303a1b545e3
AlejoZepol <alejozepol@gmail.com>
2018-08-18 11:50:09 -0500

    Cambio de tiempo mas rapido

--- intensidadBrillo/intensidadBrillo.ino
+++ intensidadBrillo/intensidadBrillo.ino
@@ -1,6 +1,6 @@
 const int led1 =10;
 
-int valorBrillo =50;
+int valorBrillo =0;
 int i;
 
 void setup() {
@@ -9,7 +9,20 @@ pinMode(led1, OUTPUT);
 void loop() {
   // put your main code here, to run repeatedly:
 
-analogWrite(led1, valorBrillo);
+for (valorBrillo=0; valorBrillo<255; valorBrillo++)
+{
+  delay (10);
+    analogWrite(led1, valorBrillo);
+  }
+  delay (1000);
+  for (valorBrillo=255; valorBrillo < 0; valorBrillo--)
+{
+  delay (10);
+    analogWrite(led1, valorBrillo);
+  }
+
+
+
 
 }
 
